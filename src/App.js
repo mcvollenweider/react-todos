811540ac6869485a375e0e97824bb5363fb08433
mcvollenweider <mcvollenweider@gmail.com>
2023-02-06 16:11:42 -0500

    added new todo button and state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
+import React, {useState} from 'react';
 import "./App.css";
 import TodoTable from "./components/TodoTable";
 
 function App() {
-  const todos = [
+
+  const [todos, setTodos] = useState([
     { rowNumber: 1, rowDescription: "Feed puppy", rowAssigned: "User One" },
     { rowNumber: 2, rowDescription: "Water plants", rowAssigned: "User Two" },
     { rowNumber: 3, rowDescription: "Make Dinner", rowAssigned: "User One" },
-    { rowNumber: 4, rowDescription: "Charge phone battery", rowAssigned: "User One" },
-  ];
+    { rowNumber: 4, rowDescription: "Charge phone battery", rowAssigned: "User One"},
+  ]);
+
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: "New Todo",
+        rowAssigned: "User Three",
+      };
+      setTodos(todos => [...todos, newTodo])
+    }
+  };
 
   return (
     <div className="mt-5 container">
       <div className="card">
         <div className="card-header">Your Todo's</div>
-        <div className="card-body">  
-        <TodoTable todos={todos}/>
+        <div className="card-body">
+          <TodoTable todos={todos} />
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add New Todo
+          </button>
         </div>
       </div>
     </div>
